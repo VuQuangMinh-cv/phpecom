70254e88f32567825171b54760667cfd29718877
VuQuangMinh-cv <at1707302002.cv@gmail.com>
2024-09-04 13:39:07 +0700

    Initial commit

---
+++ config/dbcon.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "phpecom";
+
+$con = mysqli_connect($host, $user, $password, $dbname);
+
+// if (!$connect) {
+//     echo "No Connection: " . mysqli_connect_error();
+// } else {
+//     echo "We are good to go";
+// }
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include("includes/header.php")?>
+   <h1>Hell word</h1>
+         <div class="py-5">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+
+               <?php 
+               if(isset($_SESSION['message'])) 
+               {
+               ?>    
+                  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                     <strong>Heyy!</strong> <?= $_SESSION['message']; ?>.
+                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>
+               <?php 
+                    unset($_SESSION['message']);
+                  }
+               ?>
+                  </div>
+               </div>
+            </div>
+         </div>
+   <button class="btn btn-primary">tétt</button>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <?php include("includes/footer.php")?>
+
+   
